f9bbf13d378b7dd8da937cbfce69ff6dd2e994b3
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2024-07-24 10:54:24 -0500

    main

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ console.log({
   pid,
   input,
 });
+
+h1.innerHTML = 'Patito <br> Bonito';
+h1.innerText = 'Patito <br> Feo';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = "456"
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
+console.log(img);
+
+pid.innerHTML = "";
+pid.appendChild(img);
