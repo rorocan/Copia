81b932b429024dd224bbf1da4147e5584054904a
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2024-07-25 10:18:20 -0500

    main2

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('#pid');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
+
+console.log(input.value);
 
 console.log({
   h1,
@@ -16,7 +18,7 @@ console.log({
   input,
 });
 
-h1.innerHTML = 'Patito <br> Bonito';
+h1.innerHTML = 'Patito <br> Feo';
 h1.innerText = 'Patito <br> Feo';
 // console.log(h1.getAttribute('class'));
 // h1.setAttribute('class', 'rojo');
@@ -26,6 +28,7 @@ h1.classList.remove('verde');
 // h1.classList.toggle('verde');
 // h1.classList.contains('verde');
 
+input.value = "456"
 
 const img = document.createElement('img');
 img.setAttribute('src', 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif');
